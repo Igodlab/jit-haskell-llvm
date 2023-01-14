f888bce78fe9e30b7156eaf73ca784768116d2e7
Igodlab <igodlab@gmail.com>
2023-01-13 20:23:23 -0800

    Writing parser insteal of lexical analyzer

--- src/chapter2/Parser.hs
+++ src/chapter2/Parser.hs
@@ -1,43 +1,89 @@
-module Lexer ( 
-  ) where
+module Parser where
 
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
-import           Text.Parsec.String      (Parser)
-import           Text.Parsec.Language    (emptyDef)
-import qualified Text.Parsec.Token    as Tok 
+import qualified Text.Parsec.Expr as Ex
+import qualified Text.Parsec.Token as Tok
 
-lexer :: Tok.TokenParser ()
-lexer = Tok.makeTokenParser style 
-  where
-    ops = ["+", "*", "-", ";"]
-    names = ["def", "extern"]
-    style = emptyDef { 
-                  Tok.commentLine = "#"
-                , Tok.reservedOpNames = ops
-                , Tok.reservedNames = names
-                }
+import Lexer
+import Syntax
 
-integer :: Parser Integer
-integer = Tok.integer lexer 
+binary s f assoc = Ex.Infix (reservedOp s >> return (BinOp f)) assoc
 
-float :: Parser Double
-float = Tok.float lexer 
+table = [[binary "*" Times Ex.AssocLeft,
+          binary "/" Divide Ex.AssocLeft]
+        ,[binary "+" Plus Ex.AssocLeft,
+          binary "-" Minus Ex.AssocLeft]]
 
-parens :: Parser a -> Parser a
-parens = Tok.parens lexer
+int :: Parser Expr
+int = do
+  n <- integer
+  return $ Float (fromInteger n)
 
-commaSep :: Parser a -> Parser [a]
-commaSep = Tok.commaSep lexer 
+floating :: Parser Expr
+floating = do
+  n <- float
+  return $ Float n
 
-semiSep :: Parser a -> Parser [a]
-semiSep = Tok.semiSep lexer
+expr :: Parser Expr
+expr = Ex.buildExpressionParser table factor
 
-identifier :: Parser String
-identifier = Tok.identifier lexer
+variable :: Parser Expr
+variable = do
+  var <- identifier
+  return $ Var var
 
-reserved :: String -> Parser ()
-reserved = Tok.reserved lexer 
+function :: Parser Expr
+function = do
+  reserved "def"
+  name <- identifier
+  args <- parens $ many variable
+  body <- expr
+  return $ Function name args body
 
-reservedOp :: String -> Parser ()
-reservedOp  Tok.reservedOp lexer 
+extern :: Parser Expr
+extern = do
+  reserved "extern"
+  name <- identifier
+  args <- parens $ many variable
+  return $ Extern name args
 
+call :: Parser Expr
+call = do
+  name <- identifier
+  args <- parens $ commaSep expr
+  return $ Call name args
+
+factor :: Parser Expr
+factor = try floating
+      <|> try int
+      <|> try extern
+      <|> try function
+      <|> try call
+      <|> variable
+      <|> parens expr
+
+defn :: Parser Expr
+defn = try extern
+    <|> try function
+    <|> expr
+
+contents :: Parser a -> Parser a
+contents p = do
+  Tok.whiteSpace lexer
+  r <- p
+  eof
+  return r
+
+toplevel :: Parser [Expr]
+toplevel = many $ do
+    def <- defn
+    reservedOp ";"
+    return def
+
+parseExpr :: String -> Either ParseError Expr
+parseExpr s = parse (contents expr) "<stdin>" s
+
+parseToplevel :: String -> Either ParseError [Expr]
+parseToplevel s = parse (contents toplevel) "<stdin>" s
